e9119198fbe5ff440e2d8f0e11f541d059fa3da5
Oto Soares Coelho Júnior <oto.coelho-junior@serpro.gov.br>
2020-12-14 13:45:39 -0300

    Uso de Resource Bundle

--- src/main/java/br/org/otojunior/demo/AbstractDemoRest.java
+++ src/main/java/br/org/otojunior/demo/AbstractDemoRest.java
@@ -6,7 +6,11 @@ package br.org.otojunior.demo;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.ResourceBundle;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.MessageSourceResourceBundle;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,14 +23,31 @@ import lombok.extern.slf4j.Slf4j;
  *
  */
 @Slf4j
-public class AbstractDemoRest {
+public abstract class AbstractDemoRest {
+	@Autowired
+	private ResourceBundle res;
+	
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(ValidacaoNegocioException.class)
 	public Map<String, List<ObjectError>> handleErrors(final ValidacaoNegocioException ex) {
 		List<ObjectError> errors = ex
 			.getErrors()
 			.getAllErrors();
-		errors.forEach(o -> log.error("Erro: {}", o.getCode()));
+		errors.forEach(o -> log.error(
+			"ERRO: [objectName={}, code={}, msg={}, str={}]",
+			o.getObjectName(),
+			o.getCode(),
+			o.getDefaultMessage(),
+			res.getString(o.getCode())));
 		return Collections.singletonMap("erros", errors);
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	@Bean
+	public ResourceBundle getResourceBundle() {
+		return ResourceBundle.getBundle("messages");
 	}
 }
