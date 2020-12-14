cb7ba25853284d5410ed0dec8f0a86ccda486c1a
Oto Soares Coelho Júnior <oto.coelho-junior@serpro.gov.br>
2020-12-14 12:07:37 -0300

    Melhorias no retorno do erro

--- src/main/java/br/org/otojunior/demo/rest/AbstractDemoRest.java
+++ src/main/java/br/org/otojunior/demo/rest/AbstractDemoRest.java
@@ -3,7 +3,12 @@
  */
 package br.org.otojunior.demo.rest;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -18,10 +23,11 @@ import lombok.extern.slf4j.Slf4j;
 public class AbstractDemoRest {
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(ValidacaoNegocioException.class)
-	public String handleErrors(final ValidacaoNegocioException ex) {
-		ex.getErrors()
-			.getAllErrors()
-			.forEach(o -> log.error("Erro: {}", o.getCode()));
-		return null;
+	public Map<String, List<ObjectError>> handleErrors(final ValidacaoNegocioException ex) {
+		List<ObjectError> errors = ex
+			.getErrors()
+			.getAllErrors();
+		errors.forEach(o -> log.error("Erro: {}", o.getCode()));
+		return Collections.singletonMap("erros", errors);
 	}
 }
